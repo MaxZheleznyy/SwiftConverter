e7bef3d08111d18637d7388263a36a9f4b3e8f0c
Max <maxzheleznyy@gmail.com>
2020-07-20 09:59:18 -0700

    project structure

---
+++ SwiftConverter/Support/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SwiftConverter
+//
+//  Created by Maxim Zheleznyy on 7/16/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ SwiftConverter/Support/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  SwiftConverter
+//
+//  Created by Maxim Zheleznyy on 7/16/20.
+//  Copyright © 2020 Maxim Zheleznyy. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = ContentView()
+
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
